6d0b2f9798e426be54aea24490da9ab9383d1005
pablenko <someone@somewhere.com>
2022-11-20 13:40:32 +0300

    homework

---
+++ arithmetic/isprime.go
@@ -0,0 +1,12 @@
+// Package arithmetic IsPrime function checks if a number is prime or not
+package arithmetic
+
+func IsPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Pablenkovitch/arithmetic
+
+go 1.19
